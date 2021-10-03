8f10bfd147d60cd9d2264af42ac88d3cceec302c
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-10-03 12:15:03 -0700

    basic event loop

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,53 @@
 <script lang="ts">
-	const shuffle = () => {};
+	let inMotion: boolean = false;
+
+	const shuffleClip = () => {
+		console.log('hello');
+	};
+
+	const shuffle = () => {
+		// do stuff
+		shuffleClip();
+		// otherwise call self again
+		if (inMotion) {
+			window.setTimeout(shuffle, 200);
+		}
+	};
+
+	const playback = () => {
+		if (inMotion) {
+			inMotion = false;
+		} else {
+			inMotion = true;
+			shuffle();
+		}
+	};
 </script>
 
 <!-- Button in the center of the page -->
 <div id="button_wrapper">
-	<button id="button">Funky Banana</button>
+	<button
+		id="button"
+		on:mouseover={playback}
+		on:focus={playback}
+		on:mouseout={playback}
+		on:blur={playback}>Funky Banana</button
+	>
 </div>
 
 <style lang="scss">
 	:global(body) {
 		background-color: black;
 	}
-    #button_wrapper {
-        // width: 100%;
-        // height: 100%;
-        min-height: 95vh;
+	#button_wrapper {
+		// width: 100%;
+		// height: 100%;
+		min-height: 95vh;
 
-        display: grid;
-        align-items: center;
-        justify-content: center;
-    }
+		display: grid;
+		align-items: center;
+		justify-content: center;
+	}
 	#button {
 		font-size: 2rem;
 		// font-family: '';
@@ -33,7 +61,7 @@
 
 		padding: 2rem;
 		// position: absolute;
-        left: 0;
-        right: 0;
+		left: 0;
+		right: 0;
 	}
 </style>
