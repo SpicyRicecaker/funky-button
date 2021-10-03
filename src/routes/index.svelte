58c5ba8858e7851a15c294b1b122e2a9d5c6e10b
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-10-03 12:59:51 -0700

    works but super buggy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,17 +2,23 @@
 	let inMotion: boolean = false;
 
 	const shuffleClip = () => {
-		console.log('hello');
+		// Generate 8 random numbers between 0 and 10
+		// let out = [];
+		for (let i = 1; i <= 8; i++) {
+			// generate random number
+			let num = Math.random() * 10;
+			buttonBinding.style.setProperty(`--cl-offset-${i}`, `${num}%`);
+		}
 	};
 
-	const shuffle = () => {
-		// do stuff
+	let buttonBinding: HTMLButtonElement;
+
+	function shuffle() {
 		shuffleClip();
-		// otherwise call self again
 		if (inMotion) {
 			window.setTimeout(shuffle, 200);
 		}
-	};
+	}
 
 	const playback = () => {
 		if (inMotion) {
@@ -28,6 +34,7 @@
 <div id="button_wrapper">
 	<button
 		id="button"
+		bind:this={buttonBinding}
 		on:mouseover={playback}
 		on:focus={playback}
 		on:mouseout={playback}
@@ -37,7 +44,7 @@
 
 <style lang="scss">
 	:global(body) {
-		background-color: black;
+		background-color: aliceblue;
 	}
 	#button_wrapper {
 		// width: 100%;
@@ -49,19 +56,36 @@
 		justify-content: center;
 	}
 	#button {
+		transition: 0.3s;
 		font-size: 2rem;
 		// font-family: '';
 		// must be absolutely positioned
-		background-color: white;
+		color: white;
+		background-color: black;
 		border: none;
 		outline: none;
 		&:hover {
 			cursor: pointer;
 		}
-
+		// &::after {
+		// 	background-color: white;
+		// }
 		padding: 2rem;
-		// position: absolute;
-		left: 0;
-		right: 0;
+
+		// starting from the top left corner, clockwise
+		// clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0 100%);
+		// clip-path: polygon(10% 10%, 90% 10%, 90% 90%, 10% 90%);
+		// each of the values are from 0-10, so we can just straightup insert our offset
+		// 0-10, 0-10
+		// 90-100, 0-10
+		// 90-100, 90-100
+		// 0-10, 90-100
+		// except we're adding calc(100-) in various places so we can just generate 8 numbers from 0-10 and mix those around
+		clip-path: polygon(
+			var(--cl-offset-1, 0) var(--cl-offset-2, 0),
+			calc(100% - var(--cl-offset-3, 0)) var(--cl-offset-4, 0),
+			calc(100% - var(--cl-offset-5, 0)) calc(100% - var(--cl-offset-6, 0)),
+			var(--cl-offset-7, 0) calc(100% - var(--cl-offset-8, 0))
+		);
 	}
 </style>
